2a52463efcd54a4fcefd53760a045edf19accfd5
tiffanyvongchanh <tvongchanh@gmail.com>
2026-03-18 10:25:34 +0100

    Fonctions

--- Automate.py
+++ Automate.py
@@ -1,2 +1,50 @@
+class Automaton:
 
-cccc
+    def __init__(self, num_symbols, num_states, initial_states, final_states, num_transitions, transitions):
+        self.num_symbols = num_symbols
+        self.num_states = num_states
+        self.initial_states = initial_states
+        self.final_states = final_states
+        self.num_transitions = num_transitions
+        self.transitions = transitions
+    
+    def read_txt():
+        return
+
+    def display():
+        return
+
+    def standardize():
+        return
+
+    def is_standardized():
+        return
+    
+    def determinize():
+        return
+    
+    def is_deterministic():
+        return
+
+    def complete():
+        return
+
+    def is_complete():
+        return
+
+    def minimize():
+        return
+
+    def is_minimized():
+        return
+
+    def recognize_words():
+        return
+    
+    def complementary_language():
+        return
+
+    
+
+    
+    
