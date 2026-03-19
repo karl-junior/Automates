80325cb6f23aad058a0487a8e216732fee302539
Fekoua Motcheka Karl Junior <juniorfekoua@gmail.com>
2026-03-19 19:01:09 +0100

    Refactor Automaton class and add read_txt method

--- Automate.py
+++ Automate.py
@@ -1,15 +1,41 @@
-class Automaton:
+# Creation de la classe Automate
+class Automate:
 
-    def __init__(self, num_symbols, num_states, initial_states, final_states, num_transitions, transitions):
-        self.num_symbols = num_symbols
-        self.num_states = num_states
-        self.initial_states = initial_states
-        self.final_states = final_states
-        self.num_transitions = num_transitions
-        self.transitions = transitions
-    
-    def read_txt():
-        return
+    #definition de la fonction de lecture
+    def read_txt(self,file):
+        with open(file, "r") as file :
+            automate = file.readlines()
+            self.num_symbols = int(automate[0].strip()) # initialisation du nombre de symbole
+            self.num_states = int(automate[1].strip()) # initialisation du nombre d'etats
+
+            initial_line = automate[2].strip().split()
+            self.num_initial_states = int(initial_line[0]) #initialisation du
+            self.initial_states = list(map(int,initial_line[1:]))
+
+            final_line = automate[3].strip().split()
+            self.num_final_states = final_line[0]
+            self.final_states = list(map(int,final_line[1:]))
+
+
+            self.num_transitions = int(automate[4].strip())
+            self.transitions = {}
+            if self.num_transitions != 0 :
+                for i in range(5, 5 + self.num_transitions):
+                    line = automate[i].strip()
+
+                    j= 0
+                    while line[j].isdigit() :
+                        j += 1
+
+                    debut = int(line[:j])
+                    altphabet = line[j]
+                    fin = int(line[j+1:])
+
+                    key = (debut, altphabet)
+                    if key not in self.transitions.keys():
+                        self.transitions[key] = []
+
+                    self.transitions[key].append(fin)
 
     def display():
         return
