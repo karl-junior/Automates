3a4876cfa1cf5170afb715584c16f10adafb2e42
Fekoua Motcheka Karl Junior <juniorfekoua@gmail.com>
2026-03-20 13:32:41 +0100

    Update Automate.py

--- Automate.py
+++ Automate.py
@@ -1,40 +1,41 @@
 # Creation de la classe Automate
 class Automate:
 
-    #definition de la fonction de lecture
+    # definition de la fonction de lecture
     def read_txt(self,file):
-        with open(file, "r") as file :
-            automate = file.readlines()
+        try : 
+            with open(file, "r") as file :
+                automate = file.readlines()
 
-            # initialisation du nombre de symbole de l'alphabet
-            self.num_symbols = int(automate[0].strip())
+                # initialisation du nombre de symbole de l'alphabet
+                self.num_symbols = int(automate[0].strip())
 
-            # initialisation du nombre d'etats
-            self.num_states = int(automate[1].strip()) 
+                # initialisation du nombre d'etats
+                self.num_states = int(automate[1].strip()) 
 
-            # etat initiaux
-            initial_line = automate[2].strip().split()
-            self.num_initial_states = int(initial_line[0]) # nombre d'etats initiaux
-            self.initial_states = list(map(int,initial_line[1:])) # liste des etats initiaux
+                # etat initiaux
+                initial_line = automate[2].strip().split()
+                self.num_initial_states = int(initial_line[0]) # nombre d'etats initiaux
+                self.initial_states = list(map(int,initial_line[1:])) # liste des etats initiaux
 
-            # etat finaux
-            final_line = automate[3].strip().split() 
-            self.num_final_states = final_line[0] # nombre d'etats finaux
-            self.final_states = list(map(int,final_line[1:]))# liste des etats finaux
+                # etat finaux
+                final_line = automate[3].strip().split() 
+                self.num_final_states = int(final_line[0]) # nombre d'etats finaux
+                self.final_states = list(map(int,final_line[1:]))# liste des etats finaux
 
-            # initialisation du numbre de transition
-            self.num_transitions = int(automate[4].strip())
+                # initialisation du numbre de transition
+                self.num_transitions = int(automate[4].strip())
 
-            # transitions (c'est une bibliothéque)
-            # clé = (état de depart, symbole) = liste d'etats d'arrivée
-            self.transitions = {}
+                # transitions (c'est une bibliothéque)
+                # clé = (état de depart, symbole) = liste d'etats d'arrivée
+                self.transitions = {}
 
-            if self.num_transitions != 0 :
-                for i in range(5, 5 + self.num_transitions):
-                    line = automate[i].strip()
-                    
-                    # on cherche où s'arrête l'etat de depart
-                    j= 0
+                if self.num_transitions != 0 :
+                    for i in range(5, 5 + self.num_transitions):
+                        line = automate[i].strip()
+
+                        # on cherche où s'arrête l'etat de depart
+                        j= 0
                     while line[j].isdigit() :
                         j += 1
                     
@@ -56,6 +57,8 @@ class Automate:
                     
                     # ajout de l'etat d'arrivée à la lite des transitions
                     self.transitions[key].append(fin)
+        except FileNotFoundError :
+            print("le fichier n'existe pas")
 
 
 
@@ -78,7 +81,7 @@ class Automate:
 
 
         return
-    
+
     def is_deterministic(self):
         # verification du nombre d'etats initial
         if(self.num_initial_states > 1):
